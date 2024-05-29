c3020617e4317af428f8036a976c3063e4364940
abhorya <81875643+abhorya@users.noreply.github.com>
2024-05-29 17:21:17 +0300

    Add files via upload

---
+++ security_groups_module/__init__.py
@@ -0,0 +1,1 @@
+# This file can be left empty or you can initialize your module here if needed
---
+++ security_groups_module/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'ALMASYIA User Categories',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Module for ALMASYIA user groups and categories',
+    'description': 'This module defines user groups and a category for ALMASYIA roles.',
+    'author': 'Your Name',
+    'depends': ['base'],
+    'data': [
+        'security/groups.xml',
+        # 'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': False,
+}
